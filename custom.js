268edec2e4efdfb44e321f535e9f58b202b3b0ee
lee21423 <dbwls5676@naver.com>
2024-05-10 15:55:59 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.main_slide').slick({
+        //  arrows: false,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 600,
+        dots: true,
+        pauseOnHovrt: false,
+    });
+
+    $('.header .mbtn').on('click', function () {
+        $('.header .gnb').toggleClass('on')
+    })
+})
